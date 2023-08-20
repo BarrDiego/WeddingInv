d50e411e4e0c0e23f410b343e1383e435e7c6f48
Diego <barragandiegoem@gmail.com>
2023-08-20 12:32:38 -0300

    estructura gral y prueba estilos

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,19 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const styleGral = {
+      border: '1px solid'
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={styleGral}>
+        <div>Aqui IMAGEN PORTADA</div>
+        <div>aqui transicion</div>
+        <div>aqui info</div>
+        <div>aqui registro</div>
+        <div> aqui algo mas</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
