df8932f08614cf89c6490f1931cea4c85c7c6d59
BarrDiego <barragandiegoem@gmail.com>
2023-11-16 09:03:27 -0300

    fix mas boton maps

--- src/components/LayoutInfo.jsx
+++ src/components/LayoutInfo.jsx
@@ -39,7 +39,16 @@ export const LayoutInfo = () => {
           height='500' 
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3274.0990961633815!2d-58.098787823351735!3d-34.853737270746784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2ded515d750bb%3A0x475086e5c675aa27!2sEl%20Portal%20Salon%20De%20Eventos!5e0!3m2!1ses!2sar!4v1695750577088!5m2!1ses!2sar" 
           loading="lazy">
-        </iframe>            
+        </iframe> 
+        <div className='contMap'>
+          <h2>Visualiza el mapa completo:</h2>
+          <button className='buttonMaps' onClick={()=>{
+            window.open('https://maps.app.goo.gl/4aR7LEoPYDvDLsNG6', 'Maps', 'windowFeatures="location=yes"');
+          }}>
+            Ir a Maps
+            
+          </button>
+        </div>           
         </div>       
          
     </div>
